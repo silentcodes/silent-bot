032860981bf64b4c9736f052b1fdc7622cbc1cd9
silentcodes <32486701+silentcodes@users.noreply.github.com>
2017-10-03 15:39:20 +0200

    Create warn.js
    warn command

---
+++ commands/warn.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+exports.run = (client, message, args) => {
+  let reason = args.slice(1).join(' ');
+  let user = message.mentions.users.first();
+  let modlog = client.channels.find('name', 'mod-log');
+  if (!modlog) return message.reply('I cannot find a mod-log channel');
+  if (reason.length < 1) return message.reply('You must supply a reason for the warning.');
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to warn them.').catch(console.error);
+  const embed = new Discord.RichEmbed()
+  .setColor(0x00AE86)
+  .setTimestamp()
+  .setDescription(`**Action:** Warning\n**User:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
+  return client.channels.get(modlog.id).send({embed});
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'warn',
+  description: 'Gives a warning to the mentioned person',
+  usage: 'warn [mention] [reason]'
+};
